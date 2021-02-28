3982640e7b89d70f0582b0d635afb626739fedf6
Quin Reed <qar4778@rit.edu>
2021-02-28 15:02:27 -0500

    visual observations + frame rewards

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from time import sleep
 import argparse
 import numpy as np
 import retro
@@ -7,8 +8,9 @@ from rl.agents.dqn import DQNAgent
 from rl.policy import LinearAnnealedPolicy, BoltzmannQPolicy, EpsGreedyQPolicy
 from rl.memory import SequentialMemory
 from rl.core import Processor
-from rl.callbacks import ModelIntervalCheckpoint, FileLogger
+from rl.callbacks import ModelIntervalCheckpoint, FileLogger, Callback
 import nn
+from PIL import ImageOps, Image
 
 WINDOW_LENGTH = 4
 
@@ -25,18 +27,81 @@ nb_actions = env.action_space.n
 
 # print(env.action_space)
 # print(np.unique([env.unwrapped.get_action_meaning(env.action_space.sample()) for _ in range(100)]))
-# print([env.unwrapped.get_action_meaning(action) for action in range(env.action_space.n)])
+# print([(env.unwrapped.get_action_meaning(action), action) for action in range(env.action_space.n)])
 # print(observations[8192:].sum())
 
-INPUT_SHAPE = (2048, )
+
+BYTES = 2048
+INPUT_SHAPE = (2288, )
 input_shape = (WINDOW_LENGTH, ) + INPUT_SHAPE
 
 class SMBProcessor(Processor):
-    def process_observation(self, observation):
-        processed_observation = observation[:INPUT_SHAPE[0]]
+    frame = 0
+    alive = True
+    last_location = 40
 
-        assert processed_observation.shape == INPUT_SHAPE
-        return processed_observation.astype('uint8')  # saves storage in experience memory
+    def process_step(self, observation, reward, done, info):
+        """Processes an entire step by applying the processor to the observation, reward, and info arguments.
+        # Arguments
+            observation (object): An observation as obtained by the environment.
+            reward (float): A reward as obtained by the environment.
+            done (boolean): `True` if the environment is in a terminal state, `False` otherwise.
+            info (dict): The debug info dictionary as obtained by the environment.
+        # Returns
+            The tupel (observation, reward, done, reward) with with all elements after being processed.
+        """
+        observation = self.process_observation(observation)
+        reward = self.process_reward(reward)
+        info = self.process_info(info)
+
+        # sleep(0.0166)
+        
+        if reward <= 0 and env.get_ram()[941] == self.last_location :
+            self.frame += 1
+        else:
+            self.last_location = env.get_ram()[941]
+            self.frame = 0
+
+        if (self.frame >= 300 or info['lives'] <= 1) and env.get_ram()[1904] != 2:
+            # print(self.frame)
+            reward = -5
+            self.frame = 0
+            self.last_location = 40
+            done = True
+        elif env.get_ram()[1904] == 2:
+            done = True
+            self.frame = 0
+            self.last_location = 40
+            reward = 1000
+
+        return observation, reward, done, info
+
+    def process_observation(self, observation):
+        processed_observation = observation[:BYTES]
+
+        player_pos = env.get_ram()[941]
+
+        max_x = env.get_screen().shape[1]
+        min_x = 0
+
+        left_x = player_pos - 32
+        if left_x < 0:
+            left_x = 0
+
+        right_x = player_pos + 112
+        if right_x > max_x:
+            right_x = max_x
+
+        viz_obs = env.get_screen()[:, left_x:right_x, :]
+
+        viz_obs = ImageOps.grayscale(Image.fromarray(viz_obs))
+
+        viz_obs = np.asarray(viz_obs.resize((12, 20))).flatten()
+
+        new_obs = np.concatenate((processed_observation, viz_obs))
+
+        assert new_obs.shape == INPUT_SHAPE
+        return new_obs.astype('uint8')  # saves storage in experience memory
 
     def process_state_batch(self, batch):
         # We could perform this processing step in `process_observation`. In this case, however,
@@ -46,17 +111,30 @@ class SMBProcessor(Processor):
         return processed_batch
 
     def process_reward(self, reward):
-        return np.clip(reward, -1., 1.)
+        return np.clip(reward, -1., 1.) - 1/60 # for the frame
+
+processor = SMBProcessor()
+
+class SMBCallback(Callback):
+    def __init__(self, processor):
+        super().__init__()
+        self.processor = processor
+
+    def on_episode_begin(self, episode, logs):
+        # print(self.processor.frame)
+        self.processor.frame = 0
+        # print(self.processor.frame)
+
 
 model = nn.create_model(input_shape, nb_actions)
 
 memory = SequentialMemory(limit=1000000, window_length=WINDOW_LENGTH)
-processor = SMBProcessor()
 
 policy = LinearAnnealedPolicy(EpsGreedyQPolicy(), attr='eps', 
-                            value_max=1., value_min=.1, 
-                            # value_max=.1, value_min=.1,
-                            value_test=.05, nb_steps=10000000)
+                            value_max=.5, value_min=.3, 
+                            # value_max=.5, value_min=.1,
+                            value_test=.05, nb_steps=2000000)
+                            # value_test=.05, nb_steps=5000000)
 
 dqn = DQNAgent(model=model, nb_actions=nb_actions, policy=policy,
             memory=memory, processor=processor, nb_steps_warmup=50000, 
@@ -65,27 +143,27 @@ dqn = DQNAgent(model=model, nb_actions=nb_actions, policy=policy,
 
 dqn.compile(Adam(lr=.00025), metrics=['mae'])
 
+weights_filename = 'dqn_smb1_ram+viz1.h5f'
 if args.mode == 'train':
-    weights_filename = 'dqn_smb1.h5f'
-    checkpoint_weights_filename = 'checkpoint_weights/dqn_smb1_{step}.h5f'
+    checkpoint_weights_filename = 'checkpoint_weights/ram+viz/dqn_smb1_ram+viz_{step}.h5f'
     log_filename = 'dqn_smb1_log.json'
     callbacks = [
         ModelIntervalCheckpoint(checkpoint_weights_filename, interval=500000),
-        FileLogger(log_filename, interval=100)
+        FileLogger(log_filename, interval=100),
+        SMBCallback(processor)
     ]
 
-    check_weights_file = tf.train.latest_checkpoint('./checkpoint_weights')
+    # check_weights_file = tf.train.latest_checkpoint('checkpoint_weights')
+    check_weights_file = weights_filename
     if check_weights_file:
         dqn.load_weights(check_weights_file)
 
-    dqn.fit(env, callbacks=callbacks, nb_steps=3000000, log_interval=10000, visualize=False)
+    # for _ in range(3):
+    dqn.fit(env, callbacks=callbacks, nb_steps=2000000, log_interval=10000, visualize=False)
+    dqn.save_weights('dqn_smb1_ram+viz1.h5f', overwrite=True)
+    dqn.test(env, nb_episodes=10, visualize=True)
 
-    dqn.save_weights(weights_filename, overwrite=True)
-
-    dqn.test(env, nb_episodes=10, visualize=False)
 elif args.mode == 'test':
-    weights_filename = 'dqn_smb1.h5f'
-
     dqn.load_weights(weights_filename)
 
     dqn.test(env, nb_episodes=10, visualize=True)
--- random_step.py
+++ random_step.py
@@ -2,16 +2,52 @@
 Test with random steps. Used to just make sure the emulator is working, the rom is connected appropriately
 """
 import retro
+from time import sleep
+from PIL import Image, ImageOps
+import numpy as np
 
 def main():
-    env = retro.make(game='SuperMarioBros-Nes')
+    env = retro.make(game='SuperMarioBros-Nes', use_restricted_actions=retro.Actions.DISCRETE, 
+                    ) #  obs_type=retro.Observations.RAM)
     obs = env.reset()
+    frame = 0
     while True:
-        action = env.action_space.sample()
+        frame += 1
+        action = 6
+        # action = env.action_space.sample()
         # print(action)
+        obs, rew, done, info = env.step(action)
 
-        obs, rew, done, info = env.step(env.action_space.sample())
+        player_pos = env.get_ram()[941]
+
+        max_x = env.get_screen().shape[1]
+        min_x = 0
+
+        left_x = player_pos - 32
+        if left_x < 0:
+            left_x = 0
+
+        right_x = player_pos + 112
+        if right_x > max_x:
+            right_x = max_x
+
+        viz_obs = env.get_screen()[:, left_x:right_x, :]
+
+        viz_obs = ImageOps.grayscale(Image.fromarray(viz_obs))
+
+        viz_obs = viz_obs.resize((12, 20))
+
+        viz_obs.show()
+
+        # print("frame:", frame, ":", rew, -1, rew - 1)
+
+        # print(info)
+
+
         env.render()
+
+        sleep(0.0166)
+
         if done:
             obs = env.reset()
     env.close()
